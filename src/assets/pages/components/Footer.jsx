02d9f1f46dc61bdb8d69583b432410d59480d7d8
farleyaAraujoLeite <farley2181@hotmail.com>
2022-05-09 16:09:15 -0300

    criação da pasta components e dos componentes header, body e footer

---
+++ src/assets/pages/components/Footer.jsx
@@ -0,0 +1,18 @@
+//imports react
+import React, { useState, useEffect } from 'react';
+
+//imports router
+import { Routes, Route, Link } from "react-router-dom";
+
+//imports mui
+
+
+const Footer = () => {
+    return(
+    <>
+
+    </>
+    );
+}
+
+export default Footer;
---
+++ src/assets/pages/components/Header.jsx
@@ -0,0 +1,18 @@
+//imports react
+import React, { useState, useEffect } from 'react';
+
+//imports router
+import { Routes, Route, Link } from "react-router-dom";
+
+//imports mui
+
+
+const Header = () => {
+    return(
+    <>
+    
+    </>
+    );
+}
+
+export default Header;
