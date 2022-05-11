ae363e652b04bebaf18d1e68a0b0439276418b05
farleyaAraujoLeite <farley2181@hotmail.com>
2022-05-10 21:00:11 -0300

    criação da tela de login com sucesso

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React, { useState, useEffect } from 'react';
 import { Routes, Route, Link } from "react-router-dom";
 import Home from './assets/pages/home/Home';
 import Todo from './assets/pages/todo/Todo';
+import Login from './assets/pages/login/Login'
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/todo" element={<Todo />} />
+      <Route path="/login" element={<Login />} />
     </Routes>
   );
 }
