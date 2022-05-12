a2d8e7e1fa77d295090127859cd937ec917500a7
farleyaAraujoLeite <farley2181@hotmail.com>
2022-05-11 23:19:27 -0300

    Tela de cadastro feita com sucesso

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import Home from './assets/pages/home/Home';
 import Todo from './assets/pages/todo/Todo';
 import Login from './assets/pages/login/Login';
+import Cadastro  from './assets/pages/cadastro/Cadastro'
 import './style.css';
 
 const App = () => {
@@ -11,6 +12,7 @@ const App = () => {
       <Route path="/" element={<Home />} />
       <Route path="/todo" element={<Todo />} />
       <Route path="/login" element={<Login />} />
+      <Route path="/cadastro" element={<Cadastro />} />
     </Routes>
   );
 }
