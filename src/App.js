9549c38910f776b916e781bc98023045379e4df7
Guilherme Soares <guilherme21999@gmail.com>
2022-05-25 19:05:05 -0300

    Finalizado edição de nota

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
+//imports react
 import React from 'react';
+
+//imports do router
 import { Routes, Route } from "react-router-dom";
+
+//imports de tela
 import Home from './assets/pages/home/Home';
 import Login from './assets/pages/login/Login';
 import Cadastro  from './assets/pages/cadastro/Cadastro';
 import InsertNote from './assets/pages/inserirNota/InsertNote';
 import EditarNota from './assets/pages/editarNota/EditarNota';
+
+//imports css
 import './style.css';
 
 const App = () => {
@@ -14,7 +21,7 @@ const App = () => {
       <Route path="/login" element={<Login />} />
       <Route path="/cadastro" element={<Cadastro />} />
       <Route path="/inserirNota" element={<InsertNote />} />
-      <Route path="/editarNota" element={<EditarNota />} />
+      <Route path="/editarNota/:idNota" element={<EditarNota />} />
     </Routes>
   );
 }
